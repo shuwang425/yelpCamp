081c5d7d8949201836981de5664a58a5d4c7b676
Shu <shuw425@gmail.com>
2023-06-27 11:54:28 -0500

    adding mongoosh db to the app

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const campGround = require('./models/campground')
+// Connect mongoose 
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp',{
+    useUnifiedTopology: true
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, "connection error:"));
+db.once('open', ()=>{
+    console.log("Database connected");
+})
+
+const app = express();
+
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -8,7 +21,20 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) =>{
     res.render('home')
+});
+
+app.get('/makecampground', async (req, res) =>{
+    const camp = new campGround ({
+        title: 'My Backyard',
+        description: "Cheap campimg"
+    });
+    await camp.save();
+    res.send(camp)
 })
+
+
+
+
 app.listen(3000, ()=>{
     console.log('Serving on port 3000')
 })
